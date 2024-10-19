ba9af0a808369ee7619ff9b6bc9b44ae5278a2ae
kjwsl <kjwdev01@gmail.com>
2024-10-19 12:16:51 +0900

    fix: change path

--- lua/paste/init.lua
+++ lua/paste/init.lua
@@ -3,10 +3,10 @@ local M = {}
 -- Define the function to call the Python script
 M.save_clipboard_image = function()
 	-- Prompt the user for an output path
-	local output_path = vim.fn.input("Enter output path for image: ", "~/output.png")
+	local output_path = vim.fn.input("Enter output path for image: ", "output.png")
 
 	-- Execute the Python script using the output path provided
-	local cmd = string.format("python3 ~/.local/bin/save_clipboard_image.py %s", output_path)
+	local cmd = string.format("python3 ../../scripts/paste.py %s", output_path)
 	local result = vim.fn.system(cmd)
 
 	-- Notify the user of the result
